e6c135e937e3c963f45c93f69340ebdfd1435bf3
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2020-11-23 20:53:57 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,16 @@ class PostsController < ApplicationController
     redirect_to action: :index
   end
 
+  def checked
+    post = Post.find(params[:content])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
+
 end
