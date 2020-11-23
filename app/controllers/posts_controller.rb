bca2866ce642e66f4c84ac1fa06746036a290e01
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2020-11-23 20:33:46 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,15 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
+  # def new
 
-  end
+  # end
   
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 
 end
